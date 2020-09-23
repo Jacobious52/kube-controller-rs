fe3cdb3f9fe4476643f1006aff417291e452bfae
Jacobious52 <jacobgonzalez5252@gmail.com>
2020-09-24 00:36:09 +1000

    turn into real example of creating efs

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,64 @@
-use kube_derive::CustomResource;
+use kube::CustomResource;
 use serde::{Deserialize, Serialize};
+use std::cmp::PartialEq;
 
-#[derive(CustomResource, Debug, Clone, Deserialize, Serialize)]
-#[kube(apiextensions = "v1beta1")]
-#[kube(group = "gonzalez.com", version = "v1", namespaced)]
-#[kube(status = "HelloStatus")]
-pub struct HelloSpec {
+#[derive(CustomResource, Debug, Clone, Deserialize, Serialize, PartialEq)]
+#[kube(
+    derive = "PartialEq",
+    namespaced,
+    apiextensions = "v1beta1",
+    group = "gonzalez.com",
+    version = "v1",
+    status = "EfsRequestStatus",
+    shortname = "efs",
+    printcolumn = r#"{
+        "name":"Efs Name", 
+        "type":"string", 
+        "description":"name of efs volume", 
+        "jsonPath":".spec.name"
+    }"#,
+    printcolumn = r#"{
+        "name":"FsID", 
+        "type":"string", 
+        "description":"file_system_id of efs volume request", 
+        "jsonPath":".status.file_system_id"
+    }"#,
+    printcolumn = r#"{
+        "name":"Phase", 
+        "type":"string", 
+        "description":"phase of efs volume request", 
+        "jsonPath":".status.condition.phase"
+    }"#,
+    printcolumn = r#"{
+        "name":"Reason", 
+        "type":"string", 
+        "description":"reason for efs volume phase", 
+        "jsonPath":".status.condition.reason"
+    }"#
+)]
+pub struct EfsRequestSpec {
     pub name: String,
+    pub owner: String,
 }
 
-#[derive(Deserialize, Serialize, Clone, Debug, Default)]
-pub struct HelloStatus {
-    pub message: String,
+#[derive(Deserialize, Serialize, Clone, Debug, Default, PartialEq)]
+pub struct EfsRequestStatus {
+    pub file_system_id: Option<String>,
+    pub condition: EfsRequestCondition,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
+#[serde(tag = "phase")]
+pub enum EfsRequestCondition {
+    Initialised,
+    CreatingFileSystem,
+    CreatingMountTargets,
+    Success,
+    Failed { reason: String },
+}
+
+impl Default for EfsRequestCondition {
+    fn default() -> Self {
+        EfsRequestCondition::Initialised
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 #[macro_use]
 extern crate log;
 
+use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1beta1::CustomResourceDefinition;
 use kube::{
-    api::{Meta, PatchParams, Api, PostParams, ListParams},
+    api::{Api, ListParams, Meta, PatchParams, PostParams},
     error::Error,
     Client,
 };
-use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1beta1::CustomResourceDefinition;
 use kube_runtime::controller::{Context, Controller, ReconcilerAction};
+use rusoto_efs::Efs;
 
+use rusoto_core::{Region, RusotoError};
+use rusoto_efs::{CreateFileSystemRequest, EfsClient, Tag};
+
+use futures::StreamExt;
 use serde_json::json;
 use thiserror::Error;
-use futures::StreamExt;
 use tokio::time::Duration;
 
-use kube_controller_rs::{Hello, HelloStatus};
+use kube_controller_rs::{EfsRequest, EfsRequestCondition, EfsRequestStatus};
 
 #[derive(Error, Debug)]
 #[error("{0}")]
@@ -23,32 +27,108 @@ enum ReconcileError {
     PatchStatusFailed(#[from] kube::Error),
 }
 
-async fn reconcile(hello: Hello, ctx: Context<Client>) -> Result<ReconcilerAction, ReconcileError> {
-    let client = ctx.get_ref().clone();
-    let name = Meta::name(&hello);
-    let namespace = Meta::namespace(&hello).expect("hello is namespaced");
-    let hellos = Api::<Hello>::namespaced(client, &namespace);
+struct Data {
+    client: Client,
+    efs_client: EfsClient,
+}
 
-    let new_status = serde_json::to_vec(&json!({
-        "status": HelloStatus {
-            message: format!("hello, {}", hello.spec.name)
+fn fmt_aws_error<E>(e: RusotoError<E>) -> String
+where
+    E: std::error::Error + 'static,
+{
+    match e {
+        RusotoError::Unknown(h) => h.body_as_str().to_string(),
+        _ => e.to_string(),
+    }
+}
+
+async fn create_efs(efs_request: &EfsRequest, efs_client: &EfsClient) -> EfsRequestStatus {
+    let tags = vec![
+        Tag {
+            key: String::from("Name"),
+            value: efs_request.spec.name.clone(),
+        },
+        Tag {
+            key: String::from("Owner"),
+            value: efs_request.spec.owner.clone(),
+        },
+    ];
+
+    let create_request = CreateFileSystemRequest {
+        creation_token: String::from("test"),
+        tags: Some(tags),
+        ..CreateFileSystemRequest::default()
+    };
+
+    info!(
+        "creating aws file system {} for {}",
+        efs_request.spec.name, efs_request.spec.owner
+    );
+
+    let result = efs_client.create_file_system(create_request).await;
+
+    match result {
+        Ok(fd) => EfsRequestStatus {
+            condition: EfsRequestCondition::CreatingFileSystem,
+            file_system_id: Some(fd.file_system_id),
+        },
+        Err(e) => EfsRequestStatus {
+            condition: EfsRequestCondition::Failed {
+                reason: fmt_aws_error(e),
+            },
+            file_system_id: None,
+        },
+    }
+}
+
+async fn reconcile(
+    efs_request: EfsRequest,
+    ctx: Context<Data>,
+) -> Result<ReconcilerAction, ReconcileError> {
+    // get some info before we handle the request
+    let k8s_client = ctx.get_ref().client.clone();
+    let efs_client = ctx.get_ref().efs_client.clone();
+
+    let name = Meta::name(&efs_request);
+    let namespace = Meta::namespace(&efs_request).expect("EfsRequest is namespaced");
+
+    let efs_requests = Api::<EfsRequest>::namespaced(k8s_client, &namespace);
+
+    let new_status: EfsRequestStatus = if let Some(ref status) = efs_request.status {
+        match status.condition {
+            EfsRequestCondition::Initialised => create_efs(&efs_request, &efs_client).await,
+            EfsRequestCondition::CreatingFileSystem => status.clone(),
+            EfsRequestCondition::CreatingMountTargets => EfsRequestStatus::default(),
+            EfsRequestCondition::Success => status.clone(),
+            EfsRequestCondition::Failed { reason: _ } => status.clone(),
         }
-    }))
-    .map_err(ReconcileError::SerializationFailed)?;
+    } else {
+        // no status let's make one
+        EfsRequestStatus::default()
+    };
 
-    hellos
-        .patch_status(&name, &PatchParams::default(), new_status)
-        .await
-        .map_err(ReconcileError::PatchStatusFailed)?;
+    if Some(new_status.clone()) != efs_request.status {
+        let new_status_patch = serde_json::to_vec(&json!({ "status": new_status }))
+            .map_err(ReconcileError::SerializationFailed)?;
 
-    info!("patched status for {}", name);
+        debug!(
+            "patching status with new status {:#?} for efs {}/{}",
+            new_status, namespace, name
+        );
+        efs_requests
+            .patch_status(&name, &PatchParams::default(), new_status_patch)
+            .await
+            .map_err(ReconcileError::PatchStatusFailed)?;
+    } else {
+        info!("no change for efs {}/{}", namespace, name);
+    }
 
     Ok(ReconcilerAction {
-        requeue_after: Some(Duration::from_secs(300)),
+        requeue_after: Some(Duration::from_secs(20)),
     })
 }
 
-fn error_policy(_error: &ReconcileError, _ctx: Context<Client>) -> ReconcilerAction {
+fn error_policy(_error: &ReconcileError, _ctx: Context<Data>) -> ReconcilerAction {
     ReconcilerAction {
         requeue_after: Some(Duration::from_secs(60)),
     }
@@ -59,18 +139,24 @@ async fn main() -> Result<(), kube::Error> {
     pretty_env_logger::init();
 
     let client = Client::try_default().await?;
-    let context = Context::new(client.clone());
+    let context = Context::new(Data {
+        client: client.clone(),
+        efs_client: EfsClient::new(Region::UsEast1),
+    });
 
     // apply our own crd definition to the cluster if it's not already
     let crds = Api::<CustomResourceDefinition>::all(client.clone());
-    match crds.create(&PostParams::default(), &Hello::crd()).await {
+    match crds
+        .create(&PostParams::default(), &EfsRequest::crd())
+        .await
+    {
         Ok(_) => info!("created crd in cluster"),
         Err(Error::Api(response)) if response.code == 409 => (),
         Err(e) => return Err(e),
     };
 
-    let hellos = Api::<Hello>::all(client);
-    Controller::new(hellos, ListParams::default())
+    let efs_requests = Api::<EfsRequest>::all(client);
+    Controller::new(efs_requests, ListParams::default())
         .run(reconcile, error_policy, context)
         .for_each(|res| async move {
             match res {
